655b65d3791001476d6b2506894e839acd34e737
Jose Junior <jotacsjr@gmail.com>
2018-05-15 18:44:24 +0000

    Added basic apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+#Install apache
+package 'apache' do
+	package_name 'httpd'
+	action :install
+end
+#Start and enable service
+service 'apache' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
+
